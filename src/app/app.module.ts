9587a5f186cd0f6f7159d409ae6f49cc2961fdd6
Silvio Schmidt <sili3011@web.de>
2021-02-11 00:55:21 +0100

    minor realign

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { SharedModule } from './modules/shared/shared.module';
@@ -10,7 +9,12 @@ import { NavigationComponent } from './components/navigation/navigation.componen
 import { CollectionComponent } from './components/collection/collection.component';
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent, NavigationComponent, CollectionComponent],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    NavigationComponent,
+    CollectionComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -11,6 +11,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
+import { MatSortModule } from '@angular/material/sort';
+import { MatRippleModule } from '@angular/material/core';
 import { FlexModule } from '@angular/flex-layout/flex';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
@@ -30,6 +32,8 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     MatTableModule,
     MatPaginatorModule,
     MatInputModule,
+    MatSortModule,
+    MatRippleModule,
     // Angular Flex
     FlexModule,
     FlexLayoutModule,
