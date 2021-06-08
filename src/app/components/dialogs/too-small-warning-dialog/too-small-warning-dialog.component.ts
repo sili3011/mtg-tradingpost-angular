f7c976797d5280434798381856c077ebb08b4542
Silvio Schmidt <sili3011@web.de>
2021-06-08 20:51:21 +0200

    automatically closing small size warning screen if screen gets big enough

--- src/app/components/dialogs/too-small-warning-dialog/too-small-warning-dialog.component.ts
+++ src/app/components/dialogs/too-small-warning-dialog/too-small-warning-dialog.component.ts
@@ -1,3 +1,4 @@
+import { BreakpointObserver } from '@angular/cdk/layout';
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 
@@ -8,8 +9,17 @@ import { MatDialogRef } from '@angular/material/dialog';
 })
 export class TooSmallWarningDialogComponent {
   constructor(
-    private dialogRef: MatDialogRef<TooSmallWarningDialogComponent>
-  ) {}
+    private dialogRef: MatDialogRef<TooSmallWarningDialogComponent>,
+    private breakpointObserver: BreakpointObserver
+  ) {
+    this.breakpointObserver
+      .observe(['(min-width: 1000px)'])
+      .subscribe((result) => {
+        if (result.matches) {
+          this.close();
+        }
+      });
+  }
 
   close() {
     this.dialogRef.close();
